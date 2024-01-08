d12ae5642532cc0949691d92e7c7925686f09b88
eliazmuchave <eliazardomuchave@gmail.com>
2024-01-08 10:56:10 +0200

    create project

---
+++ src/main/java/mz/misau/sisgi/SisgiApplication.java
@@ -0,0 +1,13 @@
+package mz.misau.sisgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisgiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisgiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mz/misau/sisgi/SisgiApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.misau.sisgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisgiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
